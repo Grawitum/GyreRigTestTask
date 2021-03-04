63828042c0e2518e191b80e60ea61fff1135963e
Grawitum <mackc1m@yandex.ru>
2021-03-05 00:21:31 +0300

    Task3

---
+++ GyreRigTestTask/Assets/Skripts/Task3/Task3.cs
@@ -0,0 +1,23 @@
+﻿interface ISomeInterface
+{
+    void Call();
+}
+struct SomeStruct : ISomeInterface
+{
+    public void Call()
+    { }
+}
+class SomeClass
+{
+    public void Run()
+    {
+        var someStruct = new SomeStruct();
+        SomeMethod(someStruct);
+    }
+    public void SomeMethod(ISomeInterface @interface)
+    {
+        SomeStruct unboxed = (SomeStruct)@interface;
+        unboxed.Call();
+    }
+}
+
